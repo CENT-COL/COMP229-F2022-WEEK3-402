1577c3eebbc1942d0bcdfc68ef9d9422acef8289
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-09-23 19:10:17 -0400

    Added Express, and third-party modules

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-// import third-party nodejs module Connect
-const connect = require('connect');
+// Third Party Modules
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from 'morgan';
+import session from "express-session";
+
+// ES Modules fix for __dirname 
+import path, {dirname} from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 // instantiate app-server
-const app = connect();
+const app = express();
 
 // custom middleware
 function helloWorld(req, res, next){
