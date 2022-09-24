5a159e06423dc0a71f4212b3add9d020a6772760
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-09-23 20:40:11 -0400

    Views completed

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,12 +1,20 @@
 // Index Routes
 import { Router } from 'express';
-import { helloWorld } from '../controllers/index.controller.server.js'
+import { displayHomePage, 
+    displayAboutPage, 
+    displayProjectsPage, 
+    displayServicesPage, 
+    displayContactPage } from '../controllers/index.controller.server.js'
 
 // Intanciating the router
 const router = Router();
 
 // add middleware to connect application
-router.get('/',helloWorld);
-router.get('/hello',helloWorld);
+router.get('/',displayHomePage);
+router.get('/home',displayHomePage);
+router.get('/about', displayAboutPage );
+router.get('/projects', displayProjectsPage );
+router.get('/services', displayServicesPage );
+router.get('/contact', displayContactPage );
 
 export default router;
